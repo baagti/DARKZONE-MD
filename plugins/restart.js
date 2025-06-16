63218323ec5ac18090b08ad738ed3193d5e8142c
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-16 21:37:17 +0500

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart KHAN-MD",  
+    desc: "Restart DARKZONE-MD",  
     category: "owner",  
     filename: __filename  
 },  
