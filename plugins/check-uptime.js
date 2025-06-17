87764a0606e21681d8c40f8a0107babf4bbc1197
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-17 21:53:55 +0500

    Update check-uptime.js

--- plugins/check-uptime.js
+++ plugins/check-uptime.js
@@ -119,8 +119,8 @@ ${config.DESCRIPTION}`;
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363354023106228@newsletter',
-                    newsletterName: config.OWNER_NAME || 'JawadTechX',
+                    newsletterJid: '120363416743041101@newsletter',
+                    newsletterName: config.OWNER_NAME || '𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟',
                     serverMessageId: 143
                 }
             }
