d3b85959c309926f560605d262b6a1697fccd113
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-17 22:27:23 +0500

    Update fancy-text.js

--- plugins/fancy-text.js
+++ plugins/fancy-text.js
@@ -22,7 +22,7 @@ cmd({
     }
 
     const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
-    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *Powered by JAWADTECHX*`;
+    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟*`;
 
     await conn.sendMessage(from, { text: resultText }, { quoted: m });
   } catch (error) {
