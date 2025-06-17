53335dc2dac136145adc75cb73d8547cfe658bdc
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-17 23:22:34 +0500

    Update imagescan.js

--- plugins/imagescan.js
+++ plugins/imagescan.js
@@ -75,7 +75,7 @@ cmd({
     await reply(
       `🔍 *Image Analysis Results*\n\n` +
       `${scanResponse.data.result}\n\n` +
-      `> © Powered by JawadTechX 💜`
+      `> 𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟 💜`
     );
 
   } catch (error) {
