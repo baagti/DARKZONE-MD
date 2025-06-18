b263f67abeb8802a79d56f20184961c69f8f9274
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-18 21:18:17 +0500

    Update check-weather.js

--- plugins/check-weather.js
+++ plugins/check-weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By JawadTechX*
+> *𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟*
 `;
         return reply(weather);
     } catch (e) {
