6dd276230a941bd6c93af86eee0fff5c6b5a29ec
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-18 10:08:20 +0500

    Update img-grey.js

--- plugins/img-grey.js
+++ plugins/img-grey.js
@@ -75,7 +75,7 @@ cmd({
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `> *Powered by JawadTechX*`
+      caption: `> *𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟*`
     });
 
   } catch (error) {
