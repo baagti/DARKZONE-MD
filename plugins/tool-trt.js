37f9f54776382c7cfdfa63cb43c20143ae8680b5
DARKZONE-MD <darkzonemd@gmail.com>
2025-06-18 08:42:39 +0500

    Update tool-trt.js

--- plugins/tool-trt.js
+++ plugins/tool-trt.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *JAWADTECHX-KHANX-TRANSLATION*
+        const translationMessage = `> *𝐸𝑅𝐹𝒜𝒩 𝒜𝐻𝑀𝒜𝒟-DARKZONE-TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
